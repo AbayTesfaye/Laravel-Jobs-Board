17631b2d0347f4931d0b88dc66e0d1f1a37276e6
AbayTesfaye <abaytesfaye02@gmail.com>
2024-07-31 04:08:17 -0700

    lesson for day eight about migration and database

--- database/migrations/2024_07_31_105416_create_job_listing_table.php
+++ database/migrations/2024_07_31_105416_create_job_listing_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('job_listing', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->string('salary');
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('job_listing');
     }
 };
