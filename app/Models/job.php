31c1b060520a05b46f4b2a639d2307ddf23a83d8
AbayTesfaye <abaytesfaye02@gmail.com>
2024-08-01 03:21:42 -0700

    lesson for day nine about Eloquent

--- app/Models/job.php
+++ app/Models/job.php
@@ -1,38 +1,10 @@
 <?php
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class job {
-    public static function all() : array
-    {
-      return [
-        [
-             'id'=>1,
-             'title'=>'Director',
-             'salary'=> '$40'
-        ],
-        [
-            'id'=>2,
-            'title'=>'Programmer',
-            'salary'=> '$50'
-        ],
-        [
-            'id'=>3,
-            'title'=>'Teacher',
-            'salary'=> '$30'
-       ]
-        ];
-    }
+class job extends Model {
+  protected $table = 'job_listing';
 
-    public static function find($id): array
-    {
-     $job = Arr::first(job::all(), fn($job) => $job['id'] == $id);
-
-     if(! $job){
-        abort(404);
-     }
-
-     return $job;
-    }
+  protected $fillable = ['title','salary'];
 }
