9509aadc7ec7ebc9f67f3af8d9bbe1348dbab981
AbayTesfaye <abaytesfaye02@gmail.com>
2024-08-24 11:49:33 -0700

    day #15 about Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\job;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+      
 
         // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
+        //     'first_name' => 'Abay',
+        //     'last_name'=> 'Tesfaye',
         //     'email' => 'test@example.com',
         // ]);
     }
---
+++ database/seeders/JobSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\job;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class JobSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Job::factory(10)->create();
+    }
+}
