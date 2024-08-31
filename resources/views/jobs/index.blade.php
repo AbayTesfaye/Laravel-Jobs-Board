944365bacf78e37a237c92d3fba0297e6db56664
AbayTesfaye <abaytesfaye02@gmail.com>
2024-08-31 06:46:06 -0700

    day #26 Asset bundling

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -7,7 +7,7 @@
             <a href="/jobs/{{ $job['id']}}" class="block px-4 py-6 border border-gray-200 rounded-lg">
                 <div class="font-bold text-blue-500 text-sm">{{$job->employer->name}}</div>
                 <div>
-                    <strong>{{ $job['title'] }}</strong>: payed {{ $job['salary'] }} per month.
+                    <strong class="text-laracast">{{ $job['title'] }}</strong>: payed {{ $job['salary'] }} per month.
                 </div>
 
             </a>
